da3703dae38bfac0fa972022d51bb1c48a7058d9
colomu <colomuller91@gmail.com>
2020-06-15 00:35:23 -0300

    ssl redirect

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const sslRedirect = require('heroku-ssl-redirect')
 const express = require('express')
 const port = process.env.PORT || 8080
 const app = express()
+app.use(sslRedirect())
 
 app.use(express.static(__dirname+'/dist/'))
 app.get(/.*/, (req,res) => {
